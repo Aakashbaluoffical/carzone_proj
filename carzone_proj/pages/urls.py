4ff6ab3cd7dd79d7736e6de31a5ab41024a996d7
Akash balu <aakashbaluoffical@gmail.com>
2022-03-22 11:58:53 +0530

    navbar navigation links are fixed

--- carzone_proj/pages/urls.py
+++ carzone_proj/pages/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('cars/', views.cars, name="cars"),
+    path('about/', views.about, name="about"),
+    path('service/', views.service, name="service"),
+    path('contact/', views.contact, name="contact"),
 
 ]
--- carzone_proj/pages/views.py
+++ carzone_proj/pages/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from . import views
 
+
 # Create your views here.
 def home(request):
     return render(request, "pages/home.html")
+
+
+def cars(request):
+    return render(request, "pages/cars.html")
+
+
+def about(request):
+    return render(request, "pages/about.html")
+
+
+def service(request):
+    return render(request, "pages/service.html")
+
+
+def contact(request):
+    return render(request, "pages/contact.html")
+
+def login(request):
+    return render(request, "")
