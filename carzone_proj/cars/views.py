748f8d7c35b0189bba619b8d02e40d5b94bd081c
Akash balu <aakashbaluoffical@gmail.com>
2022-03-30 13:09:06 +0530

    pagination is finished and one error is found car_image_4 has no attribute

--- carzone_proj/cars/views.py
+++ carzone_proj/cars/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import car
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 def cars(request):
-    return render(request, 'cars/cars.html')
+    cars = car.objects.order_by('-created_date')
+    paginator = Paginator(cars, 4)
+    page = request.GET.get('page')
+    page_cars = paginator.get_page(page)
+    data = {
+        'cars': page_cars,
+    }
+    return render(request, 'cars/cars.html', data)
+
+
+def car_detail(request, id):
+    single_car = get_object_or_404(car, pk=id)
+    data = {
+        'single_car': single_car,
+    }
+    return render(request, "cars/car_detail.html", data)
